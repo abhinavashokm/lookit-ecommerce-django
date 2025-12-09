1ab1f38124dbb8e58942fe10c26109c56fbddf55
Abhinav <abhirealme55@gmail.com>
2025-12-09 21:06:18 +0530

    create wallet models, add signal for creating wallet when user complete signup, add empty wallet transaction state ui

--- lookit/wallet/models.py
+++ lookit/wallet/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=2, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+class WalletTransactions(models.Model):
+    
+    class TransactionType(models.Choices):
+        CREDIT = 'credit'
+        DEBIT = 'debit'
+        
+    class TransactionSource(models.Choices):
+        REWARD = 'reward'
+        ONLINE = 'online'
+        SHOPPING = 'shopping'
+        REFUND = 'refund'
+        
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=2, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TransactionType.choices) #credit or debit
+    label = models.CharField(max_length=100) 
+    txn_source = models.CharField(max_length=100, choices=TransactionType.choices)  #from where the money came or where it gone
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- lookit/wallet/views.py
+++ lookit/wallet/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Wallet, WalletTransactions
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def wallet(request):
-    return render(request, "wallet/wallet.html")
+    wallet = Wallet.objects.get(user=request.user)
+    transactions = WalletTransactions.objects.filter(wallet=wallet)
+    
+    return render(request, "wallet/wallet.html",{"wallet":wallet, 'transactions':transactions})
