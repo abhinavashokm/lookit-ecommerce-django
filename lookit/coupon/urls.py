314b76a190f42ce1cc7fca2568fb6ff493650ec4
Abhinav <abhirealme55@gmail.com>
2025-12-10 20:31:13 +0530

    feat(coupon): add pages add coupon and edit coupon in admin coupon management section

--- lookit/coupon/urls.py
+++ lookit/coupon/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("admin/list/", views.admin_list_coupon, name="admin-list-coupons"),
+    path("admin/add-coupon", views.admin_add_coupon, name="admin-add-coupon"),
+    path("admin/edit-coupon", views.admin_edit_coupon, name="admin-edit-coupon"),
 ]
--- lookit/coupon/views.py
+++ lookit/coupon/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from core.decorators import admin_required
 
 # Create your views here.
+@admin_required
 def admin_list_coupon(request):
     return render(request, 'coupon/admin/list.html')
+
+@admin_required
+def admin_add_coupon(request):
+    return render(request, "coupon/admin/add_coupon.html")
+
+@admin_required
+def admin_edit_coupon(request):
+    return render(request, "coupon/admin/edit_coupon.html")
