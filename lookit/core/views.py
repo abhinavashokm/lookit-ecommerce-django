5e4b0cda9562556c727d2db0fc05c0eb5b5f4660
Abhinav <abhirealme55@gmail.com>
2025-12-19 13:00:03 +0530

    feat(core, offer): add a resuable utility to annotate offer price for product query sets. add offer price to landing page product cards

--- lookit/core/views.py
+++ lookit/core/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from product.models import Product
+from offer.utiils import annotate_offers
 
 
 def home(request):
     user = request.user
     new_arrivals = Product.objects.filter(is_active=True, variant__stock__gt=0).distinct().order_by('-created_at')[:8]
+    new_arrivals = annotate_offers(new_arrivals) #apply offer discounts
+    
     featured = Product.objects.filter(is_active=True, variant__stock__gt=0).distinct().order_by('created_at')[:4]
+    featured = annotate_offers(featured) #apply offer_discounts
+    
     return render(request, "core/index.html",{'user':user, 'new_arrivals': new_arrivals, 'featured':featured})
 
 
---
+++ lookit/offer/utiils.py
@@ -0,0 +1,70 @@
+from django.utils import timezone
+from django.db.models import (
+    OuterRef,
+    Subquery,
+    Max,
+    Value,
+    IntegerField,
+    ExpressionWrapper,
+    F,
+    DecimalField,
+)
+from django.db.models.functions import Round, Coalesce, Greatest
+from .models import Offer
+
+
+def annotate_offers(product_query_set):
+    """function which annotate largest available offer price to each product if available and return the query set"""
+    # sub queries for fetching offers
+    today = timezone.now().date()
+
+    product_discount_sq = (
+        Offer.objects.filter(
+            products=OuterRef('pk'),
+            is_active=True,
+            start_date__lte=today,
+            end_date__gte=today,
+        )
+        .values('products')
+        .annotate(max_discount=Max('discount'))
+        .values('max_discount')[:1]
+    )
+
+    category_discount_sq = (
+        Offer.objects.filter(
+            style=OuterRef('style'),
+            is_active=True,
+            start_date__lte=today,
+            end_date__gte=today,
+        )
+        .values('style')
+        .annotate(max_discount=Max('discount'))
+        .values('max_discount')[:1]
+    )
+
+    offer_annotated_products_set = (
+        product_query_set.annotate(
+            product_discount=Coalesce(
+                Subquery(product_discount_sq), Value(0), output_field=IntegerField()
+            ),
+            category_discount=Coalesce(
+                Subquery(category_discount_sq), Value(0), output_field=IntegerField()
+            ),
+        )
+        .annotate(
+            offer_percentage=Greatest(F('product_discount'), F('category_discount'))
+        )
+        .annotate(
+            offer_deduction_amount=ExpressionWrapper(
+                Round((F('price') * F('offer_percentage') / Value(100)), 2),
+                output_field=DecimalField(max_digits=10, decimal_places=2),
+            )
+        )
+        .annotate(
+            offer_price=ExpressionWrapper(
+                Round(F('price') - F('offer_deduction_amount'), 2),
+                output_field=DecimalField(max_digits=10, decimal_places=2),
+            )
+        )
+    )
+    return offer_annotated_products_set
