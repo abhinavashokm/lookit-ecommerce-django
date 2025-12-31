7ef035c63e754859c2a3bb57c23bbb86ac4918ea
Abhinav <abhirealme55@gmail.com>
2025-12-31 19:20:47 +0530

    fix: changes made in setting.py for hosting

--- lookit/lookit/wsgi.py
+++ lookit/lookit/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lookit.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lookit.lookit.settings")
 
 application = get_wsgi_application()
