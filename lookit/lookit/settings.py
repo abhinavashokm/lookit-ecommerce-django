93213d7c16cf3d251830240208261fc5c556a108
Abhinav <abhirealme55@gmail.com>
2025-12-31 13:08:12 +0530

    fix: properly resolve static files configuration conflict

--- lookit/lookit/settings.py
+++ lookit/lookit/settings.py
@@ -192,6 +192,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+
+STATIC_ROOT = BASE_DIR / "collected_static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
